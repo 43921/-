737b1bd573396e9c3db2f1d4de59a168cb1575e0
43921 <43921@users.noreply.github.com>
2026-09-02 18:23:37 +0800

    上传试验.py

---
+++ 试验.py
@@ -0,0 +1,40 @@
+# level = 1
+# print(level)
+# print(id(level))
+
+# a = 10.3434
+# b = 20.4343
+# a, b = b, a
+# print(round(a - b, 2))
+
+
+# 9月1号练习:
+
+# None是一个特殊的常量，表示缺少值或空值。
+# 它是NoneType数据类型的唯一值（其他编程语言可能称这个值为null、nil或undefined）。
+# 原理: 当函数不需要返回具体值时，默认返回None例如print()函数只负责打印输出，不产生新值，因此返回None
+# num1 = int(input("输入num1"))
+# num2 = int(input("输入num2"))
+# fuhao = input("请输入符号")
+# result = None  # 初始化
+# if fuhao == '+':
+#     result = num1 + num2
+#     print(result)
+# elif fuhao == '-':
+#     result = num1 - num2
+#     print(result)
+# elif fuhao == '*':
+#     result = num1 * num2
+#     print(result)
+# else:
+#     print("error")
+#
+# print(result)
+
+# 运算符
+# 计算(科学)运算符：加，减，乘，除/，取余%，幂运算**，整除//
+# 比较运算符：返回都是布尔值<,>,>=,<=,==,!=
+# 赋值运算符：普通赋值x=1,自加a+=5，
+# 逻辑运算符：and,or,
+# 成员运算符：in 返回布尔值：print("rain" in "yuan rain alvin eric")
+# 在列表应用：print("yuan" in ["123", "yuan"])
