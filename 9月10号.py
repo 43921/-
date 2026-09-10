5ccfa4e0e6813738b782aaa0d523b6f319359a1a
43921 <43921@users.noreply.github.com>
2026-09-10 22:50:36 +0800

    上传9月10号.py

---
+++ 9月10号.py
@@ -0,0 +1,16 @@
+# 字符串转列表
+zifuchuan = "a,b,c,d,e,f"
+print(zifuchuan.split(","))
+# 列表
+lie_list = ["a", "bf"]
+"""
+列表：
+1. 元素存储按顺序进行；
+2. 元素可以是任意类型且可不一致；
+3. 元素长度理论上无限制；
+4. 允许重复元素；
+5. 元素是可变数据类型
+"""
+# 修改列表中的元素
+lie_list[1] = "creg"
+print(lie_list)
